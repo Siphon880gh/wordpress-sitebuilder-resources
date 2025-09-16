d4a1c07b4f09c6e94517afc15acb81f00c3e254d
Siphon880gh <weng.f.fung@gmail.com>
2025-09-15 20:18:48 -0700

    Added tailwind and fontawesome

--- wf-wordpress-resources.php
+++ wf-wordpress-resources.php
@@ -24,19 +24,50 @@ class WF_Resources {
     }
 
     /**
-     * Enqueue admin styles
+     * Enqueue admin styles and scripts
      */
     public function enqueue_admin_styles($hook) {
         // Only load on our plugin's admin pages
         if (strpos($hook, 'wf-wordpress-resources') !== false) {
+            
+            // Local CSS file
             wp_enqueue_style(
                 'wf-resources-styles',
                 plugin_dir_url(__FILE__) . 'css/plugins.css',
                 array(),
                 '1.1.0'
             );
+    
+            // Example: CDN CSS file (Font Awesome, etc.)
+            wp_enqueue_style(
+                'wf-resources-fontawesome',
+                'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css',
+                array(),
+                '6.6.0'
+            );
+
+            // Tailwind CSS CDN with preflight disabled
+            wp_enqueue_script(
+                'wf-resources-tailwind',
+                'https://cdn.tailwindcss.com/3.4.17',
+                array(),
+                '3.4.17',
+                false
+            );
+
+            // Tailwind configuration to disable preflight
+            wp_add_inline_script(
+                'wf-resources-tailwind',
+                'tailwind.config = {
+                    corePlugins: {
+                        preflight: false,
+                    }
+                };',
+                'after'
+            );
+        
         }
-    }
+    } // enqueue_admin_styles
 
     /**
      * Add a top-level menu in the admin sidebar
@@ -63,23 +94,23 @@ class WF_Resources {
     
             <h2 class="nav-tab-wrapper">
                 <a href="?page=wf-wordpress-resources&tab=wp"
-                   class="nav-tab <?php echo $active_tab === 'wp' ? 'nav-tab-active' : ''; ?>">
+                   class="nav-tab font-light <?php echo $active_tab === 'wp' ? 'nav-tab-active bg-red-50' : ''; ?>">
                     Wordpress
                 </a>
                 <a href="?page=wf-wordpress-resources&tab=classic"
-                   class="nav-tab <?php echo $active_tab === 'classic' ? 'nav-tab-active' : ''; ?>">
+                   class="nav-tab <?php echo $active_tab === 'classic' ? 'nav-tab-active bg-red-50' : ''; ?>">
                     Classic
                 </a>
                 <a href="?page=wf-wordpress-resources&tab=gutenberg"
-                   class="nav-tab <?php echo $active_tab === 'gutenberg' ? 'nav-tab-active' : ''; ?>">
+                   class="nav-tab <?php echo $active_tab === 'gutenberg' ? 'nav-tab-active bg-red-50' : ''; ?>">
                     Gutenberg
                 </a>
                 <a href="?page=wf-wordpress-resources&tab=wpbakery"
-                   class="nav-tab <?php echo $active_tab === 'wpbakery' ? 'nav-tab-active' : ''; ?>">
+                   class="nav-tab <?php echo $active_tab === 'wpbakery' ? 'nav-tab-active bg-red-50' : ''; ?>">
                     WPBakery
                 </a>
                 <a href="?page=wf-wordpress-resources&tab=elementor"
-                   class="nav-tab <?php echo $active_tab === 'elementor' ? 'nav-tab-active' : ''; ?>">
+                   class="nav-tab <?php echo $active_tab === 'elementor' ? 'nav-tab-active bg-red-50' : ''; ?>">
                     Elementor
                 </a>
             </h2>
